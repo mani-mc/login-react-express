38a6443ed8f79fe15fabc70539c909a121219006
Manikandan <manikandan.thedev@gmail.com>
2025-10-21 12:45:18 +0530

    fix: correct case of App.css import

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Login from "../src/pages/Login"
 import Dashboard from "../src/pages/Dashboard"
-import "./app.css"
+import "./App.css"
 
 
 export default function App() {
